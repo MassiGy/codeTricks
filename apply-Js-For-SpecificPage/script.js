4d8228cb21a7f9d068b017778f9a4be00ba003de
MassiGy <ghernaoutmassi@gmail.com>
2021-06-23 23:50:39 +0100

    Adding Tricks & Tweak Code

---
+++ apply-Js-For-SpecificPage/script.js
@@ -0,0 +1,6 @@
+if (window.location.href.includes('C:/')) {
+    // do what ever you want to this page
+
+    alert('your page is a file in your C drive!!!!!');
+
+}
---
+++ show-hide-Password/script.js
@@ -0,0 +1,3 @@
+//    if (window.location.href.includes('login') || window.location.href.includes('signup')) {
+$("#password").password('toggle');
+//    }
