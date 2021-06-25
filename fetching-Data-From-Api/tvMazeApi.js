6a8a38f6403d97f1f8f898253756d8bc7a111a77
MassiGy <ghernaoutmassi@gmail.com>
2021-06-26 00:22:33 +0100

    Tv Maze Api development

--- fetching-Data-From-Api/tvMazeApi.js
+++ fetching-Data-From-Api/tvMazeApi.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 let config = { headers: { Accept: 'application/json' } }
+const exec = require('child_process').exec
 
 
 
@@ -7,11 +8,20 @@ let config = { headers: { Accept: 'application/json' } }
 const fetcher = async(query) => {
     let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`, config)
         .then((res) => {
+            res.data.length = 10;
             for (let movie of res.data) {
                 let formatedData = new Object({
                     name: movie.show.name,
                     url: movie.show.url,
                     rating: movie.score * 10,
+                })
+
+                exec(`start chrome ${movie.show.url}`, function(err) {
+                    if (err) {
+                        console.error(err)
+                    } else {
+                        console.log("success open")
+                    }
                 })
                 console.log(formatedData)
             }
