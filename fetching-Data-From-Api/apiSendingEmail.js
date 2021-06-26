25496c1775a45272f7d22434f81ab73dc8fce36d
MassiGy <ghernaoutmassi@gmail.com>
2021-06-26 15:12:36 +0100

    Add Email Validation Api

--- fetching-Data-From-Api/apiSendingEmail.js
+++ fetching-Data-From-Api/apiSendingEmail.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const mailjet = require('node-mailjet').connect(process.env.apiConnectFirstArg, process.env.apiConnectSecondArg)
 const express = require('express');
 const app = express();
-
-
+const fetch = require('node-fetch');
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -18,32 +21,36 @@ app.get('/demo', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
+    fetch(`https://api.eva.pingutil.com/email?email=${req.body.email}`, requestOptions)
+        .then(() => {
+            const request = mailjet
+                .post("send", { 'version': 'v3.1' })
+                .request({
+                    "Messages": [{
+                        "From": {
+                            "Email": req.body.email,
+                            "Name": req.body.emailUsername
+                        },
+                        "To": [{
+                            "Email": "ghernaoutmassi@gmail.com",
+                            "Name": "Massi gy"
+                        }],
+                        "Subject": req.body.emailSubject,
+                        "TextPart": req.body.emailText,
+                        "HTMLPart": "",
+                        "CustomID": ""
+                    }]
+                })
+            request
+                .then((result) => {
+                    console.log(result.body)
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                })
 
-    const request = mailjet
-        .post("send", { 'version': 'v3.1' })
-        .request({
-            "Messages": [{
-                "From": {
-                    "Email": req.body.email,
-                    "Name": req.body.emailUsername
-                },
-                "To": [{
-                    "Email": "ghernaoutmassi@gmail.com",
-                    "Name": "Massi gy"
-                }],
-                "Subject": req.body.emailSubject,
-                "TextPart": req.body.emailText,
-                "HTMLPart": "",
-                "CustomID": ""
-            }]
         })
-    request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
+        .catch(error => alert(error));
     res.redirect('/demo')
 })
 
